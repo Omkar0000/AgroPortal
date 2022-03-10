c13faa8a30497daf2575fa642416481160414c05
Omkar0000 <omkarpardeshi4470@gmail.com>
2022-03-10 17:23:53 +0530

    Project-01-Create

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">AgroPortal</a>
+      </header>
+      <main>List Of Products</main>
+    </div>
+  );
+}
+
+export default App;
